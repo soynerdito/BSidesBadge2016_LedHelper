4ba4026186dc7d3104387836cdd1bdbeb8f7ba7d
Joel Roman Sosa <joelrsosarivera@gmail.com>
2016-10-01 19:55:27 -0400

    Improved readability, used Java 8 syntax and formatted spacing

--- src/com/badgehelper/app/LedMatrix.java
+++ src/com/badgehelper/app/LedMatrix.java
@@ -3,46 +3,39 @@ package com.badgehelper.app;
 import java.util.ArrayList;
 
 public class LedMatrix {
-	private boolean mLedMatrix[][] = new boolean[8][8];
-	
-	public LedMatrix(){
-		//initialize
-		clear();
-	}
-	
-	public void set(int row, int column, boolean value ){
-		mLedMatrix[column][row] = value;
-	}
-	
-	public ArrayList<String> createCommands(){
-		int rowByte = 0x00;
-		int colByte = 0x00;
-		ArrayList<String> commands = new ArrayList<String>();
-		for (int x = 0; x < 8; x++) {
-			rowByte = 0x00;
-			colByte = 0x00;
-			for (int y = 0; y < 8; y++) {
-				if (mLedMatrix [y][x]) 
-				{
-					//This is ON
-					colByte |= 1 << (7-y);
-					rowByte |= 1 << (7-x);
-					commands.add(
-							String.format("ledMatrix.write( 0x%02x , 0x%02x );", (~rowByte) & 0xFF , colByte)
-							);
-					
-				}
-			}
-		}
-		return commands;
-	}
+    private boolean mLedMatrix[][] = new boolean[8][8];
 
-	private void clear() {
-		for (int x = 0; x < 8; x++) {
-			for (int y = 0; y < 8; y++) {
-				mLedMatrix[y][x] = false;
-			}
-		}
-		
-	}
+    public LedMatrix() {
+        //initialize
+        clearLedMatrix();
+    }
+
+    public void set(int row, int column, boolean value) {
+        mLedMatrix[column][row] = value;
+    }
+
+    public ArrayList<String> createCommands() {
+        int rowByte = 0x00;
+        int colByte = 0x00;
+        ArrayList<String> commands = new ArrayList<>();
+        for (int x = 0; x < 8; x++) {
+            rowByte = 0x00;
+            colByte = 0x00;
+            for (int y = 0; y < 8; y++) {
+                if (mLedMatrix[y][x]) {
+                    //This is ON
+                    colByte |= 1 << (7 - y);
+                    rowByte |= 1 << (7 - x);
+                    commands.add(String.format("ledMatrix.write( 0x%02x , 0x%02x );", (~rowByte) & 0xFF, colByte));
+                }
+            }
+        }
+        return commands;
+    }
+
+    private void clearLedMatrix() {
+        for (int x = 0, y = 0; x < 8 && y < 8; x++, y++) {
+            mLedMatrix[y][x] = false;
+        }
+    }
 }
--- src/com/badgehelper/app/MainApp.java
+++ src/com/badgehelper/app/MainApp.java
@@ -1,132 +1,110 @@
 package com.badgehelper.app;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Font;
-import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
-import java.io.Console;
-import java.security.cert.CollectionCertStoreParameters;
-
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class MainApp extends JFrame {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -3029273708001622954L;
-	
-	private LedMatrix mMatrix = new LedMatrix();
 
-	public static void main(String[] args) {
-		MainApp e = new MainApp();
-	    e.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    e.setSize(400, 300);
-	    e.setVisible(true);
-	  }
+    /**
+     *
+     */
+    private static final long serialVersionUID = -3029273708001622954L;
 
-	  public MainApp() {
-		  super("My new Frame");
-	    Container pane = getContentPane();
-	    pane.setLayout(new BorderLayout());
-	    pane.add(getHeader(), BorderLayout.NORTH);
-	    pane.add(getMatrixArea(), BorderLayout.CENTER);
-	    pane.add(getButtonPanel(), BorderLayout.SOUTH);
-	  }
+    private LedMatrix mMatrix = new LedMatrix();
 
-	  protected JComponent getHeader() {
-	    JLabel label = new JLabel("Badge Matrix Helper", JLabel.CENTER);
-	    label.setFont(new Font("Courier", Font.BOLD, 24));
-	    return label;
-	  }
+    public static void main(String[] args) {
+        MainApp e = new MainApp();
+        e.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        e.setSize(400, 300);
+        e.setVisible(true);
+    }
 
-	  protected JComponent getMatrixArea() {
-		  JPanel inner = new JPanel();
-		  JButton button;
-		    inner.setLayout(new GridLayout(8,8));
-		    for(int row = 0; row < 8; row++ ){
-				for( int col=0;col< 8; col++ ){
-					button = new JButton (String.format("R%dC%d", row,col));
-					
-					registerOnClickListener(button, row, col);
-					inner.add (button);
-				}
-			}
-		    return inner;
-	  }
+    public MainApp() {
+        super("My new Frame");
+        Container pane = getContentPane();
+        pane.setLayout(new BorderLayout());
+        pane.add(getHeader(), BorderLayout.NORTH);
+        pane.add(getMatrixArea(), BorderLayout.CENTER);
+        pane.add(getButtonPanel(), BorderLayout.SOUTH);
+    }
 
-	  protected JComponent getButtonPanel() {
-	    JPanel inner = new JPanel();
-	    JButton button;
-	    
-	    inner.setLayout(new GridLayout(1, 2, 10, 0));
-	    button = new JButton("Create Command");
-	    button.addActionListener(new ActionListener() {			
-			@Override
-			public void actionPerformed(ActionEvent e) {
+    protected JComponent getHeader() {
+        JLabel label = new JLabel("Badge Matrix Helper", JLabel.CENTER);
+        label.setFont(new Font("Courier", Font.BOLD, 24));
+        return label;
+    }
 
-				//mMatrix.createCommands();
-			for (String line : mMatrix.createCommands()) {
-				System.out.println(line);
-			}	
-			}
-		});
-	    inner.add(button);
-	    button = new JButton("Cancel");
-	    button.addActionListener(new ActionListener(){
+    protected JComponent getMatrixArea() {
+        JPanel inner = new JPanel();
+        JButton button;
+        inner.setLayout(new GridLayout(8, 8));
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                button = new JButton(String.format("R%dC%d", row, col));
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				closeMe();			
-			}
-	    	
-	    });
-	    inner.add(button);
-	    return inner;
-	  }
-	  
-	  protected void closeMe(){
-		  dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
-		  
-	  }
+                registerOnClickListener(button, row, col);
+                inner.add(button);
+            }
+        }
+        return inner;
+    }
 
-	  private void registerOnClickListener(JButton button, int row, int column ){
-		  button.addActionListener(new ActionListener(){
-			  private int row, column;
-			  public ActionListener init(int row, int column ){
-				  this.row = row;
-				  this.column = column;
-				  
-				  return this;
-			  }
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				onClicked((JButton)e.getSource(), row, column );				
-				
-			}
-			  
-		  }.init(row, column));
-	  }
-   Color defaultColor = null;
-	protected void onClicked(JButton button, int row, int column) {
-		
-		if( button.getBackground() == Color.blue ){
-			button.setBackground( defaultColor );
-		}else{
-			if( defaultColor ==null ){
-				defaultColor = button.getBackground();
-			}
-			button.setBackground( Color.blue );	
-		}
-		mMatrix.set(7-row, 7-column, (button.getBackground() == Color.blue));
-	}
+    protected JComponent getButtonPanel() {
+        JPanel innerJPanel = new JPanel();
+        JButton button;
+
+        innerJPanel.setLayout(new GridLayout(1, 2, 10, 0));
+        button = new JButton("Create Command");
+        button.addActionListener(e -> {
+            mMatrix.createCommands().forEach(System.out::println);
+        });
+        innerJPanel.add(button);
+        button = new JButton("Cancel");
+        button.addActionListener(e -> closeMe());
+        innerJPanel.add(button);
+        return innerJPanel;
+    }
+
+    protected void closeMe() {
+        dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+    }
+
+    private void registerOnClickListener(JButton button, int row, int column) {
+        button.addActionListener(new ActionListener() {
+            private int row, column;
+
+            public ActionListener init(int row, int column) {
+                this.row = row;
+                this.column = column;
+
+                return this;
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onClicked((JButton) e.getSource(), row, column);
+
+            }
+
+        }.init(row, column));
+    }
+
+    Color defaultColor = null;
+
+    protected void onClicked(JButton button, int row, int column) {
+
+        if (button.getBackground() == Color.blue) {
+            button.setBackground(defaultColor);
+        } else {
+            if (defaultColor == null) {
+                defaultColor = button.getBackground();
+            }
+            button.setBackground(Color.blue);
+        }
+        mMatrix.set(7 - row, 7 - column, (button.getBackground() == Color.blue));
+    }
 }
 
